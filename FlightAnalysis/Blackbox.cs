27127d41f8c7a9dd13f34f5e4d75cc4da96af573
Alex Radu <alex_radu@live.com>
2018-10-23 11:06:05 +0300

    Add project files.

---
+++ FlightAnalysis/Blackbox.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightAnalysis
+{
+    class Blackbox
+    {
+        public string controller_model;
+        public int run_time;
+        public string software_version;
+        public int run_origin;
+        public decimal gps_latitude;
+        public List<string> details_headers;
+        public string uuid;
+        public List<List<object>> details_data;
+        public string controller_application;
+        public string product_name;
+        public string version;
+        public string hardware_version;
+        public bool gps_available;
+        public string date;
+        public string product_id;
+        public decimal gps_longitude;
+        public string user;
+        public string serial_number;
+        public int total_run_time;
+        public int product_style;
+        public string jump;
+        public int crash;
+
+        public List<int> time = new List<int>();
+        public List<int> battery_level = new List<int>();
+        public List<decimal> controller_gps_longitude = new List<decimal>();
+        public List<decimal> controller_gps_latitude = new List<decimal>();
+        public List<int> flying_state = new List<int>();
+        public List<int> using_mode = new List<int>();
+        public List<int> alert_state = new List<int>();
+        public List<int> wifi_signal = new List<int>();
+        public List<bool> magneto_calibrated = new List<bool>();
+        public List<bool> product_gps_available = new List<bool>();
+        public List<decimal> product_gps_longitude = new List<decimal>();
+        public List<decimal> product_gps_latitude = new List<decimal>();
+        public List<int> product_gps_altitude = new List<int>();
+        public List<int> product_gps_sv_number = new List<int>();
+        public List<int> product_position_quality = new List<int>();
+        public List<int> product_link_quality = new List<int>();
+        public List<int> streaming_quality = new List<int>();
+        public List<int> piloting_quality = new List<int>();
+        public List<decimal> speed_vx = new List<decimal>();
+        public List<decimal> speed_vy = new List<decimal>();
+        public List<decimal> speed_vz = new List<decimal>();
+        public List<decimal> angle_phi = new List<decimal>();
+        public List<decimal> angle_theta = new List<decimal>();
+        public List<int> camera_pan = new List<int>();
+        public List<int> camera_tilt = new List<int>();
+        public List<decimal> angle_psi = new List<decimal>();
+        public List<int> altitude = new List<int>();
+        public List<decimal> speed = new List<decimal>();
+        public List<decimal> SpeedVsTheta = new List<decimal>();
+
+        public void SVT()
+        {
+            for (int i = 0; i < speed.Count; i++)
+            {
+                SpeedVsTheta.Add(speed[i] - angle_theta[i]);
+            }
+        }
+
+        public void ConvertToNatives()
+        {
+            for (int i = 0; i < details_data.Count; i++)
+            {
+                time.Add((int)details_data[i][0]);
+                battery_level.Add((int)details_data[i][1]);
+                controller_gps_longitude.Add((int)details_data[i][2]);
+                controller_gps_latitude.Add((int)details_data[i][3]);
+                flying_state.Add((int)details_data[i][4]);
+                using_mode.Add((int)details_data[i][5]);
+                alert_state.Add((int)details_data[i][6]);
+                wifi_signal.Add((int)details_data[i][7]);
+                magneto_calibrated.Add((bool)details_data[i][8]);
+                product_gps_available.Add((bool)details_data[i][9]);
+                product_gps_longitude.Add(Convert.ToDecimal(details_data[i][10]));
+                product_gps_latitude.Add(Convert.ToDecimal(details_data[i][11]));
+                product_gps_altitude.Add((int)details_data[i][12]);
+                product_gps_sv_number.Add((int)details_data[i][13]);
+                product_position_quality.Add((int)details_data[i][14]);
+                product_link_quality.Add((int)details_data[i][15]);
+                streaming_quality.Add((int)details_data[i][16]);
+                piloting_quality.Add((int)details_data[i][17]);
+                speed_vx.Add(Convert.ToDecimal(details_data[i][18]));
+                speed_vy.Add(Convert.ToDecimal(details_data[i][19]));
+                speed_vz.Add(Convert.ToDecimal(details_data[i][20]));
+                angle_phi.Add(Convert.ToDecimal(details_data[i][21]));
+                angle_theta.Add(Convert.ToDecimal(details_data[i][22]));
+                camera_pan.Add((int)details_data[i][23]);
+                camera_tilt.Add((int)details_data[i][24]);
+                angle_psi.Add(Convert.ToDecimal(details_data[i][25]));
+                altitude.Add((int)details_data[i][26]);
+                speed.Add(Convert.ToDecimal(details_data[i][27]));
+            }
+        }
+    }
+}
+
---
+++ FlightAnalysis/Program.cs
@@ -0,0 +1,107 @@
+﻿using NPlot;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+
+namespace FlightAnalysis
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //string file = File.ReadAllText(@"C:\Users\abradu\Desktop\Personal\Flights\284F4A90C57F3FA3E04E364FE6ACB925.json");
+            string file = File.ReadAllText(@"C:\Users\abradu\Desktop\Personal\Flights\20056AD50DEFACE5A912E9076752BA70.json");
+
+            Blackbox bb = new JavaScriptSerializer().Deserialize<Blackbox>(file);
+            bb.ConvertToNatives();
+            bb.SVT();
+
+            for (int i = 0; i < bb.altitude.Count; i++)
+            {
+                bb.altitude[i] = bb.altitude[i] / 1000;
+            }
+
+            NPlot.Bitmap.PlotSurface2D npSurface = new NPlot.Bitmap.PlotSurface2D(10000, 1000);
+            NPlot.LinePlot npPlot1 = new LinePlot();
+            NPlot.LinePlot npPlot2 = new LinePlot();
+            NPlot.LinePlot npPlot3 = new LinePlot();
+            NPlot.LinePlot npPlot4 = new LinePlot();
+            NPlot.LinePlot npPlot5 = new LinePlot();
+
+            npSurface.Clear();
+            npSurface.Title = "Line Graph";
+            npSurface.BackColor = System.Drawing.Color.White;
+
+            //Left Y axis grid:
+            NPlot.Grid p = new Grid();
+            NPlot.HistogramPlot h = new HistogramPlot();
+            npSurface.Add(p, NPlot.PlotSurface2D.XAxisPosition.Bottom,
+                          NPlot.PlotSurface2D.YAxisPosition.Left);
+
+            //Phi:
+            npPlot1.AbscissaData = bb.time;
+            npPlot1.DataSource = bb.angle_phi;
+            npPlot1.Label = "Phi";
+            npPlot1.Color = System.Drawing.Color.Blue;
+
+            npSurface.Add(npPlot1, NPlot.PlotSurface2D.XAxisPosition.Bottom,
+                  NPlot.PlotSurface2D.YAxisPosition.Left);
+
+            //Phi:
+            npPlot2.AbscissaData = bb.time;
+            npPlot2.DataSource = bb.angle_psi;
+            npPlot2.Label = "Psi";
+            npPlot2.Color = System.Drawing.Color.Red;
+
+            npSurface.Add(npPlot2, NPlot.PlotSurface2D.XAxisPosition.Bottom,
+                  NPlot.PlotSurface2D.YAxisPosition.Left);
+
+            //Theta:
+            npPlot3.AbscissaData = bb.time;
+            npPlot3.DataSource = bb.angle_theta;
+            npPlot3.Label = "Theta";
+            npPlot3.Color = System.Drawing.Color.Green;
+
+            npSurface.Add(npPlot3, NPlot.PlotSurface2D.XAxisPosition.Bottom,
+                  NPlot.PlotSurface2D.YAxisPosition.Left);
+
+            //Speed:
+            npPlot4.AbscissaData = bb.time;
+            npPlot4.DataSource = bb.SpeedVsTheta;
+            npPlot4.Label = "Speed";
+            npPlot4.Color = System.Drawing.Color.Black;
+
+            npSurface.Add(npPlot4, NPlot.PlotSurface2D.XAxisPosition.Bottom,
+                  NPlot.PlotSurface2D.YAxisPosition.Left);
+
+            ////Alti:
+            //npPlot5.AbscissaData = bb.time;
+            //npPlot5.DataSource = bb.altitude;
+            //npPlot5.Label = "Alt";
+            //npPlot5.Color = System.Drawing.Color.Orange;
+
+            //npSurface.Add(npPlot5, NPlot.PlotSurface2D.XAxisPosition.Bottom,
+            //      NPlot.PlotSurface2D.YAxisPosition.Left);
+
+            npSurface.Refresh();
+
+            MemoryStream memStream = new MemoryStream();
+
+            npSurface.Bitmap.Save(memStream, System.Drawing.Imaging.ImageFormat.Gif);
+
+            FileStream fs = new FileStream(@"D:\file.gif", FileMode.Create);
+
+            memStream.WriteTo(fs);
+            
+            //Console.WriteLine("Done");
+            //Console.ReadLine();
+        }
+    }
+}
